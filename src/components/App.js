5d8e7585589b12247036d3e2da4c88793bc59e82
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-03-10 20:53:21 +0900

    reduxをhooksで実装

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
-
+import { useDispatch, useSelector } from 'react-redux'
 import { increment, decrement } from '../actions'
 
-const App = (props) => {
+const App = () => {
+  const value = useSelector((state) => state.value)
+  const dispatch = useDispatch();
   return (
     <>
-      <div>count:{props.value}</div>
-      <button onClick={props.increment}>+</button>
-      <button onClick={props.decrement}>-</button>
+      <div>count:{value}</div>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </>
   );
 }
 
-// stateから必要な情報をコンポーネントにマッピングする関数
-const mapStateToProps = state => ({ value: state.count.value })
-
-// dispatch関数をコンポーネントにマッピングする関数
-const mapDispatchToProps = dispatch => ({
-  increment: () => dispatch(increment()),
-  decrement: () => dispatch(decrement())
-})
-
-// stateとacitonをコンポーネントに関連づける
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,15 @@
-import { combineReducers } from 'redux'
-import count from './count'
+import { INCREMENT, DECREMENT } from '../actions'
+const initialState = { value: 0 }
 
-export default combineReducers(({ count }))
+const count = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { value: state.value + 1 }
+    case DECREMENT:
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
+
+export default count
