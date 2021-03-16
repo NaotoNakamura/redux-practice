6772cba4adee2b11933a7541991793b73afc94a0
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-03-16 19:55:11 +0900

    reducerをまとめ

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,18 @@
 import { combineReducers } from 'redux'
-import count from './count'
+import { INCREMENT, DECREMENT } from '../actions'
+
+//コンポーネントの初期値を設定
+const initialState = { value: 0 }
+
+const count = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { value: state.value + 1 }
+    case DECREMENT:
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
 
 export default combineReducers(({ count }))
