44b8674102fea5e21ddc0917fe51627ee2d13c38
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-04-30 21:07:27 +0900

    AnotherComponent追加

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './components/App'
+import Another from './components/Another'
 import {counterSlice} from './counter/counter'
 
 const store = configureStore({reducer: counterSlice.reducer})
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
+    <Another/>
   </Provider>,
   document.getElementById('root')
 );
